771dbd2fc9dbd8a8e19d2d3ebd8419dffc96d629
vagrant <vagrant@precise32.(none)>
2015-02-19 20:50:28 +0000

    Thursday

--- routes/add.js
+++ routes/add.js
@@ -2,15 +2,18 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 
-   res.render('add', data);
-
    var newvibe = {
         "name": req.query.name,
         "project": req.query.project,
         "managment": req.query.management,
         "team": req.query.team,
         "workenvirontment": req.query.workenvironment,
+        "company": req.query.company,
+        "quality": req.query.quality,
+       
    };
 
-   data["vibes"].push(newvibe);
+   data["vibes"].unshift(newvibe);
+   res.render('company', data);
+
  };
